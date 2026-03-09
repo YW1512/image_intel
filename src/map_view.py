28b12b0e3f6af3f029773e1f0f5444382b8b6307
Shlomo Feit <11.5sfeit@gmail.com>
2026-03-09 17:41:28 +0200

    created a create_map function as per the requirements

--- src/map_view.py
+++ src/map_view.py
@@ -14,7 +14,8 @@ map_view.py - יצירת מפה אינטראקטיבית
 """
 
 import folium
-
+from folium.map import Icon
+from itertools import cycle
 
 def sort_by_time(arr):
     return sorted(arr, key=lambda item: item.get("datetime", default='')
@@ -30,7 +31,40 @@ def create_map(images_data):
     Returns:
         string של HTML (המפה)
     """
-    pass
+
+    images_w_gps = [img for img in images_data if img['has_gps']]
+
+    if not images_w_gps:
+        return "<h2>No GPS data found</h2>"
+
+    c_latitude = sum(img['latitude'] for img in images_w_gps)  / len(images_w_gps)
+    c_longitude = sum(img['longitude'] for img in images_w_gps) / len(images_w_gps)
+
+    map_view = folium.Map(location=[c_latitude, c_longitude], zoom_start=8)
+
+    color_cycle = cycle(list(Icon.color_options))
+
+    color_map = {}
+    points = []
+
+    for image in images_w_gps:
+        if image['camera_model'] not in color_map:
+            color_map[image['camera_model']] = next(color_cycle)
+
+    for image in images_w_gps:
+        points.append([image['latitude'], image['longitude']])
+        folium.Marker(
+            location=[image['latitude'], image['longitude']],
+            popup=(f'file name: {image["filename"]}\n'
+                   f'date: {image["datetime"]}\n'
+                   f'device: {image["camera_make"]} - {image["camera_model"]}'),
+            icon = folium.Icon(color=color_map[image['camera_model']])
+        ).add_to(map_view)
+
+    folium.PolyLine(points).add_to(map_view)
+
+    return map_view._repr_html_()
+
 
 
 
@@ -41,10 +75,16 @@ if __name__ == "__main__":
          "has_gps": True, "camera_make": "Samsung", "camera_model": "Galaxy S23",
          "datetime": "2025-01-12 08:30:00"},
         {"filename": "test2.jpg", "latitude": 31.7683, "longitude": 35.2137,
+         "has_gps": True, "camera_make": "Apple", "camera_model": "Galaxy S23",
+         "datetime": "2025-01-13 09:00:00"},
+        {"filename": "test2.jpg", "latitude": 29.7683, "longitude": 35.2137,
          "has_gps": True, "camera_make": "Apple", "camera_model": "iPhone 15 Pro",
          "datetime": "2025-01-13 09:00:00"},
+        {"filename": "test2.jpg", "latitude": 30.7683, "longitude": 35.2137,
+         "has_gps": True, "camera_make": "Apple", "camera_model": "iPhone 15 Pro",
+         "datetime": "2025-01-13 09:00:00"}
     ]
-    html = create_map(fake_data)
+    html = create_map(sort_by_time(fake_data))
     with open("test_map.html", "w", encoding="utf-8") as f:
         f.write(html)
     print("Map saved to test_map.html")
